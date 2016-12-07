ede37684349a69f28141be14d71a5596c1a10693
Vitalii Petrushka <vpetrushka52@gmail.com>
2016-12-07 21:50:11 +0200

    final changes added

--- app/components/grid/grid.tsx
+++ app/components/grid/grid.tsx
@@ -11,14 +11,19 @@ function GridController(GridService, LogService) {
    this.data = GridService.getData().data;
    this.dataFormats = GridService.getData().dataFormats;
    this.adding_data = false;
-   this.addNewField = (valid) => {
+   this.addNewField = () => {
       //noinspection RedundantIfStatementJS
       if(this.adding_data) {
          this.newField._id = this.data[this.data.length - 1]._id + 1;
-         console.log(this.newField);
-         GridService.addNewField(this.newField);
-         this.adding_data = false;
-         this.newField = {};
+         if(this.newField.BirthDate) {
+            let _date = new Date(this.newField.BirthDate);
+            if(isNaN(_date.valueOf())) { LogService.error("Invalid Date"); }
+            else this.newField.BirthDate = _date;
+         }
+         if(GridService.addNewField(this.newField)) {
+            this.adding_data = false;
+            this.newField = {};
+         }
       } else {
          this.adding_data = true;
       }
--- app/services/Grid.service.ts
+++ app/services/Grid.service.ts
@@ -7,10 +7,14 @@ function GridServiceController($http, LogService) {
 
    //cat be any of structure
    var dataFormats = [
-      {Name: "Login", DataType: "string", IsRequired: true},
+      {Name: "Login", DataType: "String", IsRequired: true},
       {Name: "BirthDate", DataType: "Date", IsRequired: true},
-      {Name: "Age", DataType: "number", IsRequired: false}
+      {Name: "Age", DataType: "Number", IsRequired: false}
    ];
+
+   Date.prototype.toString = function() {
+      return this.toDateString();
+   };
 
    var data = [
       {_id: 1, Login: "User1", BirthDate: new Date("1996/03/14"), Age: 20},
@@ -25,13 +29,11 @@ function GridServiceController($http, LogService) {
          data
       };
    };
-   this.addNewField = function(field):void {
-      if(field) {
-         data.push(field);
-         //API call: $http.post(url, body);
-      } else {
-         LogService.error("add new user error");
-      }
+   this.addNewField = function(obj) {
+      if(obj && validate(obj)) {
+         data.push(obj);
+         return true;
+      } else return false;
    };
    this.deleteField = function(id: number):void {
       if(LogService.confirm()) {
@@ -44,6 +46,39 @@ function GridServiceController($http, LogService) {
    this.modifyField = function(_id:number, user):void {
       //API call: $http.put(url, body);
    };
+   function validate(obj) {
+      try {
+         dataFormats.forEach((field) => {
+            let curr_field = obj[field.Name];
+            if(curr_field) {
+               if(!checkType(curr_field, field.DataType)) {
+                  LogService.error("Type Error at " + field.Name + " : " + getType(curr_field) + " but expected " + field.DataType);
+                  throw new TypeError();
+               }
+            }
+            else if (field.IsRequired) {
+               LogService.error(field.Name + " is required!");
+               throw new FieldRequiredException();
+            } else { curr_field = null; }
+         });
+      } catch (e) {
+         return false;
+      }
+
+      //if everything's fine return true;
+      return true;
+   }
+   function checkType(field, type) {
+      return  getType(field) === type;
+   }
+   function getType(field) {
+      return Object.prototype.toString.call(field).slice(8, -1);
+   }
+   //custom exceptions
+   function FieldRequiredException(message = null) {
+      Error.apply(this, arguments);
+   }
+   FieldRequiredException.prototype = Object.create(Error.prototype);
    //this.calculate_age = function(birthdate:string):number {
    //   return Math.floor((Date.now() - new Date(birthdate).getTime()) / (1000 * 3600 * 24 * 365));
    //};
